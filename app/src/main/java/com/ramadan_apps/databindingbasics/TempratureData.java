1868cfff0d144d1239846e3b134d3c9b8019a0ec
elsamet <eng.mahmoudramadan2012@gmail.com>
2018-02-09 17:05:43 +0200

    make temprature observable

--- app/src/main/java/com/ramadan_apps/databindingbasics/TempratureData.java
+++ app/src/main/java/com/ramadan_apps/databindingbasics/TempratureData.java
@@ -1,10 +1,13 @@
 package com.ramadan_apps.databindingbasics;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by Mahmoud Ramadan on 2/8/18.
  */
 
-public class TempratureData {
+public class TempratureData extends BaseObservable {
 
     public TempratureData(String location ,String temp){
         this.location = location;
@@ -14,19 +17,23 @@ public class TempratureData {
     private String location;
     private  String celisus;
 
+    @Bindable
     public String getLocation() {
         return location;
     }
 
     public void setLocation(String location) {
         this.location = location;
+        notifyPropertyChanged(BR.location);
     }
 
+    @Bindable
     public String getCelisus() {
         return celisus;
     }
 
     public void setCelisus(String celisus) {
         this.celisus = celisus;
+        notifyPropertyChanged(BR.celisus);
     }
 }
